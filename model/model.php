bcc4336ba61f03c6c43a5f4a2529e977296244ed
Sebastian <sebastian.pehlke@haw-hamburg.de>
2017-11-27 11:48:05 +0100

    added table produktionsbereich

---
+++ model/model.php
@@ -0,0 +1,41 @@
+ <?php
+
+$servername = "pstud0.mt.haw-hamburg.de";
+$username = "acc036";
+$password = "acc036";
+$dbname = "acc036";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT Produktionsbereich_ID, Produktionsbereich FROM Produktionsbereiche";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+	$rows = array();
+    while($row = $result->fetch_assoc()) {
+		$rows[] = $row;	
+    }	
+} else {
+    echo "0 results";
+}
+
+echo '<div id=data>';
+	
+echo '</div>';
+?> 
+<script language="Javascript" type="text/javascript">
+
+  var jsvar = '<?php echo json_encode($rows); ?>';
+  console.log(jsvar);
+  var div = document.getElementById('data');
+  div.innerHTML = jsvar;
+</script>
+
+
+
