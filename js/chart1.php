2aa51db4bc2e8898f5892de338d64807b0090c35
Sebastian <sebastian.pehlke@haw-hamburg.de>
2017-11-28 15:59:27 +0100

    added and implemented sort function

--- js/chart1.php
+++ js/chart1.php
@@ -69,10 +69,14 @@
             myHorizontalBar.options.title.text = "Energieverbrauch in TJ";
             horizontalBarChartData.datasets[0].label = "Energieverbrauch";
             data = [];
+            produktionsbereiche = <?php echo json_encode($produktionsbereiche);  ?>;
+            produktionsbereiche.shift();
             for (let j = 0; j < energieverbräuche.length; j++){
                     data.push(energieverbräuche[j]["Jahr_" + jahr]);  
             }
-            horizontalBarChartData.datasets[0].data = data;
+            var result = sort(produktionsbereiche, data);
+            horizontalBarChartData.labels = Object.keys(result);
+            horizontalBarChartData.datasets[0].data = Object.values(result);
             window.myHorizontalBar.update();              
     });
     document.getElementById('option2').addEventListener('click', function() {
@@ -80,43 +84,68 @@
             myHorizontalBar.options.title.text = "CO2-Emission in t";
             horizontalBarChartData.datasets[0].label = "CO2-Emission";
             data = [];
+            produktionsbereiche = <?php echo json_encode($produktionsbereiche);  ?>;
+            produktionsbereiche.shift();
             for (let k = 0; k < emissionen.length; k++){
                     data.push(emissionen[k]["Jahr_" + jahr]);  
             }
-            horizontalBarChartData.datasets[0].data = data;
+            var result = sort(produktionsbereiche, data);
+            horizontalBarChartData.labels = Object.keys(result);
+            horizontalBarChartData.datasets[0].data = Object.values(result);
             window.myHorizontalBar.update();
     });
         
-    //dropdown button function
-    
+    //dropdown buttons onclick function
     for (let i = 0; i < ddButtons.length; i++){
         ddButtons[i].id = i;
-        
         ddButtons[i].onclick = function(){
-            console.log("button lenght: " + ddButtons[i]);
+            //sets button text to selected jahr
             jahr = ddButtons[i].innerHTML;
-            
             dropdownMenu2.innerHTML = jahr;
-            //todo: daten anpassen
+            //resets data und produktionsbereiche
             data = [];
+            produktionsbereiche = <?php echo json_encode($produktionsbereiche);  ?>;
+            produktionsbereiche.shift();
+            //checkt ob energie oder co2 angezeigt werden
             if (energie){
-                console.log("energie daten");
                 for (let j = 0; j < energieverbräuche.length; j++){
                     data.push(energieverbräuche[j]["Jahr_" + jahr]);  
                 }
             } else if (!energie){
-                console.log("co2 daten");
                 for (let k = 0; k < emissionen.length; k++){
                     data.push(emissionen[k]["Jahr_" + jahr]);  
                 }
             }
-            horizontalBarChartData.datasets[0].data = data;
+            //sortiert produktionsbereiche und daten
+            var result = sort(produktionsbereiche, data);
+            //setzt sortierte labels 
+            horizontalBarChartData.labels = Object.keys(result);
+            //setzt sortierte daten
+            horizontalBarChartData.datasets[0].data = Object.values(result);
+            //updatet chart
             window.myHorizontalBar.update();
         }
     }
         
-      
-        
+ //sortiert nach daten und gibt ein objekt zurück mit {"Produktionsbereich" => "123456"}     
+function sort (keys, daten){
+    var result = {};
+    var length = keys.length;
+    for(var n = length-1; n>=0; n--){
+        for(var m = length-n; m > 0; m--){
+            if (parseInt(daten[m]) > parseInt(daten[m-1])){
+                var tmp = daten[m];
+                daten[m] = daten[m-1];
+                daten[m-1] = tmp;
+                var tmp2 = keys[m];
+                keys[m] = keys[m-1];
+                keys[m-1] = tmp2;
+            }
+        }
+    }
+    keys.forEach((key, i) => result[key] = parseInt(daten[i]));
+    return result;
+}        
 
 
 
